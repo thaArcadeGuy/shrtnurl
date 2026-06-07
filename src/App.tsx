3e068bd558eb3ba180b2860823121cdba500277e
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-06-07 03:31:54 +0300

    feat(auth): update App Routes with dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Landing from "./pages/Landing"
 import Signup from "./pages/Signup"
-import Signin from "./pages/SignIn"
+import Signin from "./pages/Signin"
+import Dashboard from "./pages/Dashboard"
+import ProtectedRoute from "./components/ProtectedRoute"
 
 function App() {
   return (
@@ -10,6 +12,11 @@ function App() {
         <Route path="/" element={<Landing />} /> 
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   )
