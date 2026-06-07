a06b8f853c2be7b1e85b7492a682e990c5bc92eb
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-06-07 15:34:31 +0300

    feat(config): configure Convex with Clerk

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ClerkProvider } from '@clerk/clerk-react' 
+import { ConvexProvider, ConvexReactClient } from 'convex/react'
 import './index.css'
 import App from './App.tsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const convexUrl = import.meta.env.VITE_CONVEX_URL
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY in .env")
 }
 
+if (!convexUrl) {
+  throw new Error("Missing VITE_CONVEX_URL")
+}
+
+const convex = new ConvexReactClient(convexUrl)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <ConvexProvider client={convex}>
+        <App />
+      </ConvexProvider>
     </ClerkProvider>
   </StrictMode>,
 )
